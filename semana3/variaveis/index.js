cbaee7ad7abf07b928729b1d52c8b9224af2c657
Alice Souza <alicepsouzaba1998@gmail.com>
2020-07-27 13:28:06 -0300

    Exercício 1 de Interpretação

---
+++ semana3/variaveis/index.js
@@ -0,0 +1,7 @@
+// EXERCÍCIOS DE INTERPRETAÇÃO:
+
+// 1.
+// Temos duas variáveis, "a" e "b", valendo 10 cada uma. Primeiramente será impressa 
+// a variável "b" que vale 10. Depois o valor de "b" será mudado para 5. Logo 
+// depois as duas variáveis são impressas, "a" e "b", valendo 10 e 5 respectivamente.
+
