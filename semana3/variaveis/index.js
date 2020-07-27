1b7dda21cfac3c8e246f65f93fc71965f2fd2c73
Alice Souza <alicepsouzaba1998@gmail.com>
2020-07-27 13:44:07 -0300

    Exercício 1 de Escrita

--- semana3/variaveis/index.js
+++ semana3/variaveis/index.js
@@ -6,4 +6,25 @@
 // depois as duas variáveis são impressas, "a" e "b", valendo 10 e 5 respectivamente.
 
 // 2.
-// 
+// Serão impressos os valores: 10, 10 e 10.
+
+// EXERCÍCIOS DE ESCRITA:
+
+// 1.
+
+let nome;
+let idade;
+console.log(typeof nome);
+console.log(typeof idade);
+
+// Foi impresso "undefined", porque a variável foi declarada, mas está sem valor,
+// indefinida, sem significado.
+
+nome = prompt ("Digite seu nome");
+idade = prompt ("Digite sua idade");
+console.log(typeof nome);
+console.log(typeof idade);
+
+// Foi impresso "string" para as duas variáveis, pois agora o usuário dá um valor a elas.
+
+console.log("Olá " + nome + ", " + "você tem " + idade + " anos.");
