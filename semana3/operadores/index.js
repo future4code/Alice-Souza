f37c03fdf6012d96a6d79d1a1ba5e08b6e819d99
Alice Souza <alicepsouzaba1998@gmail.com>
2020-07-28 12:19:24 -0300

    Exercício 1 de Escrita

--- semana3/operadores/index.js
+++ semana3/operadores/index.js
@@ -13,3 +13,12 @@
 // d. 3
 // e. [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 // f. 9
+
+// EXERCÍCIOS DE ESCRITA:
+
+// 1.
+let idadeUsuario = Number(prompt("Qual sua idade?"));
+let idadeAmiga = Number(prompt("Qual a idade da sua melhor amiga?"));
+console.log("Sua idade é maior do que a da sua melhor amiga?", idadeUsuario > idadeAmiga);
+console.log("Diferença da sua idade para a da sua melhor amiga: ", idadeUsuario - idadeAmiga);
+
