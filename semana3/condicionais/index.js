01b180c004c5adde9ea0cd038bdb16e9f563cff4
Alice Souza <alicepsouzaba1998@gmail.com>
2020-07-29 13:47:49 -0300

    Exercício 1 de Interpretação

---
+++ semana3/condicionais/index.js
@@ -0,0 +1,8 @@
+// EXERCÍCIOS DE INTERPRETAÇÃO:
+
+// 1.
+// O código pede um número ao usuário e transforma a string em number.
+// Ele verifica se o número é par ou ímpar. 
+// Se for par, retorna "Passou no teste."
+// Se for ímpar, retorna "Não passou no teste." 
+
