d496f7da2036b4069b1ce6604090458e9b5fcd85
Alice Souza <alicepsouzaba1998@gmail.com>
2020-07-29 13:54:30 -0300

    Exercício 2 de Interpretação

--- semana3/condicionais/index.js
+++ semana3/condicionais/index.js
@@ -6,3 +6,8 @@
 // Se for par, retorna "Passou no teste."
 // Se for ímpar, retorna "Não passou no teste." 
 
+// 2.
+// a. O código serve para determinar o preço da fruta que o usuário escolher.
+// b. Se for escolhido Maçã será impresso: "O preço da fruta Maçã é R$ 2.25".
+// c. Se for Pêra (sem o break) será impresso: "O preço da fruta Pêra é R$ 5.5 R$ 5".
+
