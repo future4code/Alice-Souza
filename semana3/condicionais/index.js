43b13e9f29b27db218dc39f2e4fd1d065f3a86d2
Alice Souza <alicepsouzaba1998@gmail.com>
2020-07-29 14:01:30 -0300

    Exercício 3 de Interpretação

--- semana3/condicionais/index.js
+++ semana3/condicionais/index.js
@@ -11,3 +11,9 @@
 // b. Se for escolhido Maçã será impresso: "O preço da fruta Maçã é R$ 2.25".
 // c. Se for Pêra (sem o break) será impresso: "O preço da fruta Pêra é R$ 5.5 R$ 5".
 
+// 3.
+// a. Pedindo um número ao usuário e transformando ele num Number.
+// b. Se for o número 10, será impresso: "Esse número passou no teste" e um erro. 
+// Se for o -10, retorna um erro.
+// c. Sim, haverá erro, pois o escopo filho não compartilha sua variável com o pai.
+
