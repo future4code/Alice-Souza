135fc392a76dfb39ba99c1cb7f280014e5ef0432
Alice Souza <alicepsouzaba1998@gmail.com>
2020-07-30 14:16:33 -0300

    Desafio 1

--- semana3/loops/index.js
+++ semana3/loops/index.js
@@ -9,3 +9,12 @@
 // a. 19, 21, 23, 25, 27, 30
 // b. for..of não é suficiente.
 
+// DESAFIO 1:
+
+// Se fosse digitado 4, apareceria 4 linhas de zero(s), com um zero a mais a \
+// cada linha. Isso porque temos uma quantidade inicial vazia, à qual vai \
+// ser atribuída +1 a cada loop ao número de linhas com a string "0".
+// Temos um loop que "diz": enquanto a quantidade inicial for menor que a \
+// quantidade digitada, faça o que está no for, que é o seguinte: enquanto \
+// asteriscos (índice) for menor que a qunatidade atual +1, some um ao índice \
+// E a cada repetição desse for, será atribuído um zero a cada linha.
