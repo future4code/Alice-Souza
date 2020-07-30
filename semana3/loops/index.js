6f62dff6e48425fd002f9eeebf7fdce33c478475
Alice Souza <alicepsouzaba1998@gmail.com>
2020-07-30 13:42:52 -0300

    Exercício 1 de Interpretação

---
+++ semana3/loops/index.js
@@ -0,0 +1,7 @@
+// EXERCÍCIOS DE INTERPRETAÇÃO:
+
+// 1.
+// Atribui um valor inicial como zero. Depois, enquanto o índice for menor que \
+// 5, repete o laço. E soma/atribui 1 ao valor a cada repetição. Portanto, 5 \
+// será o valor impresso no console.
+
