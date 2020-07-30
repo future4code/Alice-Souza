602266a87f7c5a9d78cb590d3b7c47e490b569b8
Alice Souza <alicepsouzaba1998@gmail.com>
2020-07-30 14:00:29 -0300

    Exercício 2 de Interpretação

--- semana3/loops/index.js
+++ semana3/loops/index.js
@@ -5,3 +5,7 @@
 // 5, repete o laço. E soma/atribui 1 ao valor a cada repetição. Portanto, 5 \
 // será o valor impresso no console.
 
+// 2.
+// a. 19, 21, 23, 25, 27, 30
+// b. for..of não é suficiente.
+
