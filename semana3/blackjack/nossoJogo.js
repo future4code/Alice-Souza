77852b28ef2ea80233f7320c3a57c0fdba6677af
Alice Souza <alicepsouzaba1998@gmail.com>
2020-08-01 11:26:42 -0300

    Etapa 1 - Nosso Jogo

--- semana3/blackjack/nossoJogo.js
+++ semana3/blackjack/nossoJogo.js
@@ -10,3 +10,33 @@
  * 
  * 
  */
+
+// alert ('Bem-vindo(a) ao jogo de Blackjack!');
+// if (confirm('Quer iniciar uma nova rodada?')){
+//   // Carta usuário
+//   const carta1 = comprarCarta();
+//   let carta1User = carta1.texto;
+//   const carta2 = comprarCarta();
+//   let carta2User = carta2.texto;
+//   let pontuacaoUser = carta1.valor + carta2.valor;
+//   // Carta Computador
+//   const carta3 = comprarCarta();
+//   let carta3PC = carta3.texto;
+//   const carta4 = comprarCarta();
+//   let carta4PC = carta4.texto;
+//   let pontuacaoPC = carta3.valor + carta4.valor;
+//   // Imprimindo resultados
+//   console.log(`Usuário - cartas: ${carta1User} ${carta2User} - pontuação ${pontuacaoUser}`);
+//   console.log(`Computador - cartas: ${carta3PC} ${carta4PC} - pontuação ${pontuacaoPC}`);
+//   // Resultado do vencedor
+//   if (pontuacaoUser > pontuacaoPC) {
+//      console.log('O usuário ganhou!');
+//   } else if (pontuacaoPC > pontuacaoUser) {
+//      console.log('O computador ganhou!')
+//   } else {
+//      console.log('Empate!');
+//   };
+// } else { // Caso não queira jogar
+//   console.log('O jogo acabou!');
+// };
+
