412be3694241f86548ad9b182f27b16f20399d4d
Alice Souza <alicepsouzaba1998@gmail.com>
2020-08-03 16:09:41 -0300

    Exercício 1 de Interpretação

---
+++ semana4/funcoes/index.js
@@ -0,0 +1,12 @@
+// EXERCÍCIOS DE INTERPRETAÇÃO:
+
+//
+// // 1:
+//
+// a. 10 
+//    50
+// b. Não iria aparecer nada, pois em algum lugar tem que ter o console.log. Nesse\
+// caso poderia colocar o console.log no lugar de return.
+
+
+
