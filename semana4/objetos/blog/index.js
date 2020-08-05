0a0dfbdc10c761cec05f3a6c781fa3245a94b5ca
Alice Souza <alicepsouzaba1998@gmail.com>
2020-08-05 14:25:20 -0300

    Inseri o campo de imagem e funcionalidades

--- semana4/objetos/blog/index.js
+++ semana4/objetos/blog/index.js
@@ -1,22 +1,25 @@
 const criarPost = () => {
   // Pegando cada elemento:
   let tituloPost = document.getElementById('titulo-post');
+  let imagemPost = document.getElementById('imagem-post');
   let autorPost = document.getElementById('autor-post');
   let conteudoPost = document.getElementById('conteudo-post');
   // Armazenando seu valor em um objeto:
   const post = {
     titulo: tituloPost.value,
     autor: autorPost.value,
-    conteudo: conteudoPost.value
+    conteudo: conteudoPost.value,
+    imagem: imagemPost.value
   };
   // Atribuindo seus valores para a seção do post:
   let containerPost = document.getElementById('container-de-posts');
-  containerPost.innerHTML += `<h1>${post.titulo}</h1> <pre>${post.conteudo}</pre> <h3>${post.autor}</h3>`;
+  containerPost.innerHTML += `<h1>${post.titulo}</h1> <img src=${post.imagem}> <pre>${post.conteudo}</pre> <h3>${post.autor}</h3>`;
   // Limpando o formulário:
-  const arrayPost = [tituloPost, autorPost, conteudoPost];
+  const arrayPost = [tituloPost, imagemPost, autorPost, conteudoPost];
   arrayPost[0].value = '';
   arrayPost[1].value = '';
   arrayPost[2].value = '';
+  arrayPost[3].value = '';
 };
 
 
