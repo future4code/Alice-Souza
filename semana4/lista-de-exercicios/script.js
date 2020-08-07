6061f3e1a02d34db627b80b041faec4cf31f709f
Alice Souza <alicepsouzaba1998@gmail.com>
2020-08-07 12:02:02 -0300

    Exercício 1 de Interpretação

---
+++ semana4/lista-de-exercicios/script.js
@@ -0,0 +1,12 @@
+// // // EXERCÍCIOS DE INTERPRETAÇÃO:
+
+// -------------------------------------
+
+//   1:
+//--------
+// Converte o valor em Dólar para Reais.
+// O valor em dólar é o argumento que o usuário passa na função. E num prompt é
+// pedido o valor da cotação do dólar. Então é retornado um valor em reais que 
+// é igual ao valor em dólar (passado no argumento) vezes a cotação. 
+// Será impresso o valor em reais.
+
