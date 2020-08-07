b80448e8f45b058b2d929d05193b26be2a1c6197
Alice Souza <alicepsouzaba1998@gmail.com>
2020-08-07 12:10:44 -0300

    Exercício 2 de Interpretação

--- semana4/lista-de-exercicios/script.js
+++ semana4/lista-de-exercicios/script.js
@@ -3,10 +3,21 @@
 // -------------------------------------
 
 //   1:
-//--------
+// -------
 // Converte o valor em Dólar para Reais.
 // O valor em dólar é o argumento que o usuário passa na função. E num prompt é
 // pedido o valor da cotação do dólar. Então é retornado um valor em reais que 
 // é igual ao valor em dólar (passado no argumento) vezes a cotação. 
 // Será impresso o valor em reais.
+
+//   2:
+// -------
+// Pede ao usuário qual tipo de investimento quer fazer e o valor e então diz
+// o quanto esse investimento vai render. A função pega o tipo de investimento
+// e o valor como argumentos e coloca numa condicional, exemplo, se for poupança
+// o valor após investimento (que é o valor retornado) será igual ao valor vezes
+// 1.03, daí tem outros tipos de investimentos que são calculados da mesma
+// forma (CDB, ações, etc...). Se o tipo de investimento não estiver nas condi-
+// cionais, é emitido um alert. E no final, como dito, irá retornar o valor após
+// o investimento. Em seguida o usuário pode fazer quantas simulações quiser.
 
