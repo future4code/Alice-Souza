905fa0d95036162039d798e5fceb41f7371918b3
Alice Souza <alicepsouzaba1998@gmail.com>
2020-08-07 22:22:19 -0300

    Exercício 3 de Funções de Array

--- semana4/lista-de-exercicios/script.js
+++ semana4/lista-de-exercicios/script.js
@@ -290,6 +290,30 @@
 // });
 // console.log(paresEImpares);
 
+//  3:
+// -------
+// const pessoas = [
+// 	{ nome: "Paula", idade: 12, altura: 1.8},
+// 	{ nome: "João", idade: 20, altura: 1.3},
+// 	{ nome: "Pedro", idade: 15, altura: 1.9},
+// 	{ nome: "Luciano", idade: 22, altura: 1.8},
+// 	{ nome: "Artur", idade: 10, altura: 1.2},
+// 	{ nome: "Soter", idade: 70, altura: 1.9}
+// ];
+// // ------
+// // // a:
+// const pessoasPermitidas = pessoas.filter(pessoa => {
+//   return pessoa.altura >= 1.5 && (pessoa.idade >= 14 && pessoa.idade <= 60); 
+// });
+// console.log(pessoasPermitidas);
+// // ------
+// // // b:
+// const pessoasNaoPermitidas = pessoas.filter(pessoa => {
+//   return pessoa.altura < 1.5 || (pessoa.idade < 14 || pessoa.idade > 60); 
+// });
+// console.log(pessoasNaoPermitidas);
+
+
 
 
 
