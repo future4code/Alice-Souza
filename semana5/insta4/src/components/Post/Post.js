67d53e3ba7a965e1612701549c3dbd6e9abaa40f
Alice Souza <alicepsouzaba1998@gmail.com>
2020-08-12 13:05:15 -0300

    Fiz alguns ajustes no código do post

--- semana5/insta4/src/components/Post/Post.js
+++ semana5/insta4/src/components/Post/Post.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Post.css';
 import {IconeComContador} from '../IconeComContador/IconeComContador';
+import {SecaoComentario} from '../SecaoComentario/SecaoComentario';
 import iconeCoracaoBranco from '../../img/favorite-white.svg';
 import iconeCoracaoPreto from '../../img/favorite.svg';
 import iconeComentario from '../../img/comment_icon.svg';
-import {SecaoComentario} from '../SecaoComentario/SecaoComentario';
 
 export class Post extends React.Component {
   state = {
@@ -38,26 +38,28 @@ export class Post extends React.Component {
     if(this.state.comentando) {
       componenteComentario = <SecaoComentario aoEnviar={this.aoEnviarComentario}/>;
     };
-    return <div className={'post-container'}>
-      <div className={'post-header'}>
-        <img className={'user-photo'} src={this.props.fotoUsuario} alt={'Imagem do usuario'}/>
-        <p>{this.props.nomeUsuario}</p>
+    return (
+      <div className={'post-container'}>
+        <div className={'post-header'}>
+          <img className={'user-photo'} src={this.props.fotoUsuario} alt={'Imagem do usuario'}/>
+          <p>{this.props.nomeUsuario}</p>
+        </div>
+        <img className={'post-photo'} src={this.props.fotoPost} alt={'Imagem do post'}/>
+        <div className={'post-footer'}>
+          <IconeComContador
+            icone={iconeCurtida}
+            onClickIcone={this.onClickCurtida}
+            valorContador={this.state.numeroCurtidas}
+          />
+          <IconeComContador
+            icone={iconeComentario}
+            onClickIcone={this.onClickComentario}
+            valorContador={this.state.numeroComentarios}
+          />
+        </div>
+        {componenteComentario}
       </div>
-      <img className={'post-photo'} src={this.props.fotoPost} alt={'Imagem do post'}/>
-      <div className={'post-footer'}>
-        <IconeComContador
-          icone={iconeCurtida}
-          onClickIcone={this.onClickCurtida}
-          valorContador={this.state.numeroCurtidas}
-        />
-        <IconeComContador
-          icone={iconeComentario}
-          onClickIcone={this.onClickComentario}
-          valorContador={this.state.numeroComentarios}
-        />
-      </div>
-      {componenteComentario}
-    </div>
+    );
   };
 };
 export default Post
