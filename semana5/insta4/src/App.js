943d31638dd91837fd82d3d0a41ff112b9e71dc1
Alice Souza <alicepsouzaba1998@gmail.com>
2020-08-13 19:26:43 -0300

    Fiz o form para criar novo post

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -20,20 +20,61 @@ class App extends React.Component {
         fotoUsuario: 'https://picsum.photos/id/1/50/50',
         fotoPost: 'https://picsum.photos/id/2/200/150'
       }
-    ]
+    ],
+    valorInputNomeUser: '',
+    valorInputFotoUser: '',
+    valorInputFotoPost: ''
+  };
+  criarPost = () => {
+    const novoPost = {
+      nomeUsuario: this.state.valorInputNomeUser,
+      fotoUsuario: this.state.valorInputFotoUser,
+      fotoPost: this.state.valorInputFotoPost
+    };
+    const novosPosts = [...this.state.posts, novoPost];
+    this.setState({
+      posts: novosPosts 
+    });
+  };
+  onChangeInputNome = e => {
+    this.setState({ valorInputNomeUser: e.target.value });
+  };
+  onChangeInputUser = e => {
+    this.setState({ valorInputFotoUser: e.target.value });
+  };
+  onChangeInputPost = e => {
+    this.setState({ valorInputFotoPost: e.target.value });
   };
   render() {
     const postsRender = this.state.posts.map(post => {
       return (
-      <Post
-        nomeUsuario={post.nomeUsuario}
-        fotoUsuario={post.fotoUsuario}
-        fotoPost={post.fotoPost}
-      />
+        <Post
+          nomeUsuario={post.nomeUsuario}
+          fotoUsuario={post.fotoUsuario}
+          fotoPost={post.fotoPost}
+        />
       )
     })
     return (
       <div className={'app-container'}>
+        <FormPost>
+          <input
+            value={this.state.valorInputNomeUser}
+            onChange={this.onChangeInputNome}
+            placeholder={"Seu nome"}
+          />
+          <input
+            value={this.state.valorInputFotoUser}
+            onChange={this.onChangeInputUser}
+            placeholder={"URL da sua foto para perfil"}
+          />
+          <input
+            value={this.state.valorInputFotoPost}
+            onChange={this.onChangeInputPost}
+            placeholder={"URL da sua foto para o post"}
+          />
+          <button onClick={this.criarPost}>Criar Post</button>
+        </FormPost>
         {postsRender}
       </div>
     );
