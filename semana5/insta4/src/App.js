5eab3180f0fff97d0f5efd02a337368199e9e808
Alice Souza <alicepsouzaba1998@gmail.com>
2020-08-13 19:05:11 -0300

    Fiz a 1º implementação (Passar os posts para array no state)

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -3,24 +3,38 @@ import './App.css';
 import Post from './components/Post/Post';
 
 class App extends React.Component {
+  state = {
+    posts: [
+      {
+        nomeUsuario: 'Beatriz Julian',
+        fotoUsuario: 'https://picsum.photos/id/7/50/50',
+        fotoPost: 'https://picsum.photos/id/10/200/150'
+      },
+      {
+        nomeUsuario: 'André Turing',
+        fotoUsuario: 'https://picsum.photos/id/3/50/50',
+        fotoPost: 'https://picsum.photos/id/4/200/150'
+      },
+      {
+        nomeUsuario: 'Maria Jackson',
+        fotoUsuario: 'https://picsum.photos/id/1/50/50',
+        fotoPost: 'https://picsum.photos/id/2/200/150'
+      }
+    ]
+  };
   render() {
+    const postsRender = this.state.posts.map(post => {
+      return (
+      <Post
+        nomeUsuario={post.nomeUsuario}
+        fotoUsuario={post.fotoUsuario}
+        fotoPost={post.fotoPost}
+      />
+      )
+    })
     return (
       <div className={'app-container'}>
-        <Post
-          nomeUsuario={'Paula'}
-          fotoUsuario={'https://picsum.photos/id/7/50/50'}
-          fotoPost={'https://picsum.photos/id/10/200/150'}
-        />
-        <Post
-          nomeUsuario={'André'}
-          fotoUsuario={'https://picsum.photos/id/3/50/50'}
-          fotoPost={'https://picsum.photos/id/4/200/150'}
-        />
-        <Post
-          nomeUsuario={'Maria'}
-          fotoUsuario={'https://picsum.photos/id/1/50/50'}
-          fotoPost={'https://picsum.photos/id/2/200/150'}
-        />
+        {postsRender}
       </div>
     );
   };
