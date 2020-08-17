18172aeb3b693c4b99d65d13e052a832573c1f1b
Alice Souza <alicepsouzaba1998@gmail.com>
2020-08-17 17:47:28 -0300

    Final do formulário

--- semana6/labenu-forms/src/components/Final.js
+++ semana6/labenu-forms/src/components/Final.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Titulo = styled.h2`
+  color: #63625f;
+`
+
 export class Final extends React.Component {
   render() {
     return (
-      <div></div>
+      <div>
+        <Titulo>O formulário acabou :D</Titulo>
+        <p>Muito obrigado por participar! Entraremos em contato.</p>
+      </div>
     );
   };
 };
