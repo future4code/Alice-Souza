825b04946bdd22ac82d2a38c926385db479667f0
Alice Souza <alicepsouzaba1998@gmail.com>
2020-08-26 16:03:40 -0300

    Página de cadastro

--- semana7/cadastro-labenusers/src/components/CadastroUsers.jsx
+++ semana7/cadastro-labenusers/src/components/CadastroUsers.jsx
@@ -1,22 +1,49 @@
 import React from 'react';
-import {ContainerInputs} from './Styled';
+import axios from 'axios';
+import {ContainerInputs, ButtonMudaPagina, ButtonCadastra} from './Styled';
+import {baseURLCreate, Authorization} from './APIConsts';
 
 class CadastroUsers extends React.Component{
+  state = {
+    valueName: '',
+    valueEmail: ''
+  };
+  onChangeName = e => {
+    this.setState({valueName: e.target.value})
+  };
+  onChangeEmail = e => {
+    this.setState({valueEmail: e.target.value})
+  };
+  createUsers = () => {
+    const body = {
+      name: this.state.valueName,
+      email: this.state.valueEmail
+    };
+    axios.post (baseURLCreate, body, Authorization)
+    .then(response => {
+      alert('Cadastro realizado com sucesso!')
+    }).catch (error => {
+      alert('Desculpe, aconteceu um ERRO no cadastro!')
+    });
+  };
   render() {
     return (
       <ContainerInputs>
+        <ButtonMudaPagina 
+          onClick={this.props.clickUsersList}
+        >Ver lista de usuários</ButtonMudaPagina>
         <h1>Cadastro Labenusers:</h1>
         <input
-          value={this.props.valueName}
-          onChange={this.props.changeName}
+          value={this.state.valueName}
+          onChange={this.onChangeName}
           placeholder={'Seu nome'}
         /> <br></br>
         <input
-          value={this.props.valueEmail}
-          onChange={this.props.changeEmail}
+          value={this.state.valueEmail}
+          onChange={this.onChangeEmail}
           placeholder={'Seu email'}
         /> <br></br>
-        <button onClick={this.props.clickSend}>Enviar</button>
+        <ButtonCadastra onClick={this.createUsers}>Cadastrar</ButtonCadastra> 
       </ContainerInputs>
     );
   };
