796f0b28469d608dc7da590b40cb2f13081f8fc1
Alice Souza <alicepsouzaba1998@gmail.com>
2020-08-27 22:15:11 -0300

    Condicionais para abrir as páginas

--- semana7/public-APIs-integrations/src/App.js
+++ semana7/public-APIs-integrations/src/App.js
@@ -1,10 +1,49 @@
 import React from 'react';
+import Home from './components/Home';
+import Bored from './components/Bored';
+import TheStarWars from './components/TheStarWars';
 
 class App extends React.Component{
+  state = {
+    page: 'home'
+  };
+  openPageBored = () => {
+    this.setState({page: 'bored'});
+  };
+  openPageStarWars = () => {
+    this.setState({page: 'star-wars'});
+  };
+  backPageHome = () => {
+    this.setState({page: 'home'});
+  };
   render() {
+    const renderPage = () => {
+      if (this.state.page === 'home') {
+        return (
+          <Home
+            clickOpenBored={this.openPageBored}
+            clickOpenStarWars={this.openPageStarWars}
+            clickOpenPicsum={this.openPagePicsum}
+          />
+        )
+      } else if (this.state.page === 'bored') {
+        return (
+          <Bored
+            clickHome={this.backPageHome}
+            clickNext={this.openPageStarWars}
+          />
+        )
+      } else if (this.state.page === 'star-wars') {
+        return (
+          <TheStarWars
+            clickPrevious={this.openPageBored}
+          />
+        )
+      };
+    };
      return (
-      <div className="App">
-      
+      <div className={'app-container'}>
+        {renderPage()}
       </div>
     );
   };
