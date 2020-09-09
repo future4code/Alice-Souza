c1699e1027928d8dac619b02bd52a3b99113a4ba
Alice Souza <alicepsouzaba1998@gmail.com>
2020-09-09 17:40:21 -0300

    Arrumei a estrutura para começar

--- semana9/pokeApi-useEffect/src/App.js
+++ semana9/pokeApi-useEffect/src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import PokeCard from './components/PokeCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  state = {
+    pokeList: [],
+    pokeName: ""
+  };
 
+  componentDidMount = () => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
+      .then(response => {
+        this.setState({ pokeList: response.data.results });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  changePokeName = event => {
+    this.setState({ pokeName: event.target.value });
+  };
+
+    return (
+      <div className="App">
+        {/* evento onChange chama função toda vez que o usuário 
+        escolhe um novo pokemon no dropdown */}
+        <select onChange={this.changePokeName}>
+          <option value={""}>Nenhum</option>
+          {/* renderizando a lista de pokemons como opções do select */}
+          {this.state.pokeList.map(pokemon => {
+            return (
+              <option key={pokemon.name} value={pokemon.name}>
+                {pokemon.name}
+              </option>
+            );
+          })}
+        </select>
+        {/* expressão booleana que renderiza o componente PokeCard,
+        caso o valor de pokeName, no estado, seja true */}
+        {this.state.pokeName && <PokeCard pokemon={this.state.pokeName} />}
+      </div>
+    );
+};
 export default App;
