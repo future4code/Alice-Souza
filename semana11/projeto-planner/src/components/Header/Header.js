3def3055ebe9af3be1dd3849b17196378273998a
Alice <alicepsouzaba1998@gmail.com>
2020-09-26 22:50:14 -0300

    Created the structure (folders and files)

---
+++ semana11/projeto-planner/src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+  
+  return(
+
+    <div>
+      <div>
+        <h1>PlanLabe</h1>
+        <h3>Crie aqui suas tarefas semanais</h3>
+      </div>
+    </div>
+
+  )
+
+}
