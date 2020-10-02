45d213149128d5eae6f25de009a5c289cbb34a63
Alice <alicepsouzaba1998@gmail.com>
2020-10-02 16:30:04 -0300

    Screen routing

---
+++ semana12/labeddit/src/router/goToPages.js
@@ -0,0 +1,16 @@
+export const goToHomePage = (history) => {
+  history.push('/')
+}
+
+export const goToSignUpPage = (history) => {
+  history.push('/sign-up')
+}
+
+export const goToPostDetails = (history) => {
+  history.push('/post-details')
+}
+
+export const goToFeedPage = (history) => {
+  history.push('/feed')
+}
+
