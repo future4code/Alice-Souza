3b15b7794b1c2fde062fad83582774a76314a06a
Alice <alicepsouzaba1998@gmail.com>
2020-10-04 12:22:49 -0300

    Null token pages

---
+++ semana12/labeddit/src/screens/SignUpPage/styles.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.div`
+  width: 100%;
+  height: 70px;
+  background: #a3fdff;
+  display: flex;
+  align-items: center;
+  box-shadow: 0 0 3px gray;
+  > div {
+    width: 85%;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+
+    > h1 {
+      margin: 0;
+      font-family: 'BenchNine', sans-serif;
+      color: #1f2063;
+    }
+    > button {
+      width: 90px;
+      outline: none;
+      border: none;
+      box-shadow: 0px 0px 4px gray;
+      cursor: pointer;
+    }
+  }
+`
+export const FormContainer = styled.form`
+  margin-top: 65px;
+  text-align: center;
+  > input {
+    width: 250px;
+    border: none;
+    outline: none;
+    padding: 10px;
+    box-shadow: 1px 1px 3px gray;
+    margin-bottom: 10px;
+  }
+  > button {
+    width: 270px;
+    outline: none;
+    padding: 10px;
+    margin-top: 8px;
+  }
+`
