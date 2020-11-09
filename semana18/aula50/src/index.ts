cc6e845b890f691fd442f6d6aa7336b432b610cc
Alice <alicepsouzaba1998@gmail.com>
2020-11-09 14:56:10 -0300

    Initial project structure

---
+++ semana18/aula50/src/index.ts
@@ -0,0 +1,36 @@
+// Import libs
+import express from "express";
+import knex from "knex";
+import cors from "cors";
+import dotenv from "dotenv";
+import { AddressInfo } from "net";
+
+// Connection
+export const connection = knex({
+  client: 'mysql',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: 3306
+  }
+})
+
+// Configurations
+dotenv.config()
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+// Endpoints
+
+// Function rotate
+const server = app.listen(process.env.PORT || 3003, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Server is running in http://localhost:${address.port}`);
+  } else {
+    console.error(`Failure upon starting server.`);
+  }
+})
